47d7d554336b70656956d0ac714904b0f2fb7e13
Slawomir Was <was.slawek@gmail.com>
2023-11-08 12:21:15 +0100

    first commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    //public string Index()
+    //{
+    //    return "This is my default action...";
+    //}
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    //public string Welcome(string elo, int numTimes = 1)
+    //{
+    //    return HtmlEncoder.Default.Encode($"Hello {elo}, NumTimes is: {numTimes}");
+    //}
+
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello" + name;
+        ViewData["NumTimes"] = numTimes;
+
+        return View();
+    }
+
+
+}
